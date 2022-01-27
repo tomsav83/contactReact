d377004cd1a93692b20cba6a9f3884eed7f0e019
Tomsav83 <tomsav83@gmail.com>
2022-01-27 17:04:19 +0000

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,162 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import './App.scss'
 
 function App() {
+  const [state, setState] = useState({
+    name: '',
+    email: '',
+    subject: '',
+    message: '',
+  })
+
+  const { name, email, subject, message } = state
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (!name || !email || !subject || !message) {
+      toast.error('Please provide value in each input field')
+    } else {
+      firebaseDB.child('contacts').push(state)
+      setState({ name: '', email: '', subject: '', message: '' })
+      toast.success('Form Submitted Successfully')
+    }
+  }
+
+  const handleInputChange = (e) => {
+    let { name, value } = e.target
+    setState({ ...state, [name]: value })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <section className='contact-section'>
+      <div className='container'>
+        <ToastContainer position='top-center' />
+        <div className='row justify-content-center'>
+          <div className='col-md-10'>
+            <div className='wrapper'>
+              <div className='row no-gutters'>
+                <div className='col-md-6'>
+                  <div className='contact-wrap w-100 p-lg-5 p-4'>
+                    <h3 className='mb-4'>Send us a message</h3>
+                    <form
+                      id='contactForm'
+                      className='contactForm'
+                      onSubmit={handleSubmit}
+                    >
+                      <div className='row'>
+                        <div className='col-md-12'>
+                          <div className='form-group'>
+                            <input
+                              type='text'
+                              className='form-control'
+                              name='name'
+                              placeholder='Name'
+                              onChange={handleInputChange}
+                              value={name}
+                            />
+                          </div>
+                        </div>
+                        <div className='col-md-12'>
+                          <div className='form-group'>
+                            <input
+                              type='email'
+                              className='form-control'
+                              name='email'
+                              placeholder='Mail'
+                              onChange={handleInputChange}
+                              value={email}
+                            />
+                          </div>
+                        </div>
+                        <div className='col-md-12'>
+                          <div className='form-group'>
+                            <input
+                              type='text'
+                              className='form-control'
+                              name='subject'
+                              placeholder='Subject'
+                              onChange={handleInputChange}
+                              value={subject}
+                            />
+                          </div>
+                        </div>
+                        <div className='col-md-12'>
+                          <div className='form-group'>
+                            <textarea
+                              type='text'
+                              className='form-control'
+                              name='message'
+                              placeholder='Message'
+                              cols='30'
+                              rows='6'
+                              onChange={handleInputChange}
+                              value={message}
+                            ></textarea>
+                          </div>
+                        </div>
+                        <div className='col-md-12'>
+                          <div className='form-group'>
+                            <input
+                              type='submit'
+                              value='Send Message'
+                              className='btn btn-primary'
+                            />
+                          </div>
+                        </div>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+                <div className='col-md-6 d-flex align-items-stretch'>
+                  <div className='info-wrap w-100 p-lg-5 p-4 img'>
+                    <h3>Contact us</h3>
+                    <p className='mb-4'>
+                      We're open to any suggestions or just to have a chat
+                    </p>
+                    <div className='dbox w-100 d-flex align-items-center'>
+                      <div className='icon d-flex align-items-center justify-content-center'>
+                        <span className='fa fa-map-marker'></span>
+                      </div>
+                      <div className='text pl-3'>
+                        <p>
+                          <span>Address:</span> 33 Elton Avenue, Blundellsands,
+                          Liverpool, L23 8UW
+                        </p>
+                      </div>
+                    </div>
+                    <div className='dbox w-100 d-flex align-items-center'>
+                      <div className='icon d-flex align-items-center justify-content-center'>
+                        <span className='fa fa-phone'></span>
+                      </div>
+                      <div className='text pl-3'>
+                        <p>
+                          <span>Phone:</span>
+                          <a href='tel:+34 948 760 078'> +34 948 760 078</a>
+                        </p>
+                      </div>
+                    </div>
+                    <div className='dbox w-100 d-flex align-items-center'>
+                      <div className='icon d-flex align-items-center justify-content-center'>
+                        <span className='fa fa-paper-plane'></span>
+                      </div>
+                      <div className='text pl-3'>
+                        <p>
+                          <span>Mail: </span>
+                          <a href='mailto:elcamino@correos.com'>
+                            elcamino@correos.com
+                          </a>
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
 }
 
-export default App;
+export default App
---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+// import { initializeApp } from 'firebase/app'
+// import 'firebase/database'
+
+// const firebaseConfig = {
+//   apiKey: 'AIzaSyANltsl37EpYnufC_5b3YvEqtIwEHC5YYI',
+//   authDomain: 'contactpagereact.firebaseapp.com',
+//   projectId: 'contactpagereact',
+//   storageBucket: 'contactpagereact.appspot.com',
+//   messagingSenderId: '725618187015',
+//   appId: '1:725618187015:web:63160dadde49c9a6d203a2',
+// }
+
+// const fireDb = initializeApp(firebaseConfig)
+
+// export default fireDb.database().ref()
